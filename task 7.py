a3230c0ba772576ca764efa8304406cf1dbe59a4
sk04293 <54892741+sk04293@users.noreply.github.com>
2020-06-20 16:18:05 +0500

    added circle

--- task 7.py
+++ task 7.py
@@ -60,4 +60,7 @@ def path(start, step, angle):
 step_sample = [choice([0, 0.5, 1]) for _ in range(10000)]
 angle_sample = [uniform(low=0.0, high=360.0) for _ in range(10000)]
 
+circle1=plt.Circle((0,0),100,color='g', alpha= 0.2)
+plt.gcf().gca().add_artist(circle1)
+
 path([0, 0], step_sample, angle_sample)
