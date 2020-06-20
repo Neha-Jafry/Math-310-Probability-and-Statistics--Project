7398453a650459f3c4f9e0cef540b23b05a41bae
Neha-Jafry <51692007+Neha-Jafry@users.noreply.github.com>
2020-06-20 15:17:31 +0500

    task2

--- task 2.py
+++ task 2.py
@@ -11,14 +11,12 @@ def path(start1, start2):
     i=0
     path1=[start1]
     path2=[start2]
-    avg_dist=0
     while curr2-curr1 !=0:
         p=[choice([-1,1], p=[0.5, 0.5]), choice([-1,1], p=[0.5, 0.5])]
         curr1+=p[0]
         curr2+=p[1]
         path1.append(curr1)
         path2.append(curr2)
-        avg_dist+=(curr2-curr1)
         i+=1
     return path1, path2, i-1
 
@@ -27,8 +25,16 @@ plt.plot(s_path[0])
 plt.plot(s_path[1])
 plt.show()
 
-avg=0
+n=[]
 for _ in range(100):
-    avg+=(path(1, 5)[2]/100)
-# avg/=100
-print(avg)
+    n.append(path(1, 5)[2])
+plt.hist(n)
+plt.show()
+
+print(n)
+m=[]
+for i in n:
+    if i < 500:
+        m.append(i)
+plt.hist(m)
+plt.show()
